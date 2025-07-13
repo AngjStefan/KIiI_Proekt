bba96aec91a9ae5a01bd3249bcdee339ac235e8a
AngjStefan <sangjelkovski@yahoo.com>
2025-07-14 00:10:19 +0200

    Update WebConfig.java

--- car-app-backend/src/main/java/finki/ukim/mk/carappbackend/WebConfig.java
+++ car-app-backend/src/main/java/finki/ukim/mk/carappbackend/WebConfig.java
@@ -1,3 +1,7 @@
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.context.annotation.Configuration;
+
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
     @Override
