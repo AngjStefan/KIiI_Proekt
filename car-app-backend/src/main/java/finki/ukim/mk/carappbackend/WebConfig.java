484a2c995138c02a6af958e3b2c902b258be2bc1
AngjStefan <sangjelkovski@yahoo.com>
2025-07-14 00:03:49 +0200

    Create WebConfig.java

---
+++ car-app-backend/src/main/java/finki/ukim/mk/carappbackend/WebConfig.java
@@ -0,0 +1,10 @@
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/api/**")
+            .allowedOrigins("http://car-app-frontend", "http://localhost:5173")
+            .allowedMethods("GET", "POST", "PUT", "DELETE")
+            .allowedHeaders("*");
+    }
+}
